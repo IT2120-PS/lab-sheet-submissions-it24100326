31554fff6c091b035abe8c23398603bb9242ac9f
it24100326 <it24100326@my.sliit.lk>
2025-09-16 23:41:32 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24100326_Lab07.R
@@ -0,0 +1,13 @@
+setwd("C:\\Users\\hp\\OneDrive\\Desktop\\IT24100326")
+
+#1)
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+#2)
+pexp(2, rate = 1/3)
+
+#3)i)
+pnorm(130, mean = 100, sd = 15, lower.tail = FALSE)
+
+#3)ii)
+qnorm(0.95, mean = 100, sd = 15)
